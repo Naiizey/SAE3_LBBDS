8125f6a606dac6f0e271bbc4f6a24b0b7108c0f6
Mathis <72527059+Taximin@users.noreply.github.com>
2022-10-20 12:10:50 +0200

    Add files via upload

---
+++ page_acceuil/header.php
@@ -0,0 +1,21 @@
+<div>
+    <a class="Alizon" href="index.html"> <!-- Lien accueil -->
+        <img src="./images/logo.png" alt="logoAlizon" title="Accueil" class="logoAlizon">
+        <h1>Alizon</h1>
+    </a>
+    <div class="divRecherche">
+        <input class="champsRecherche" type="text" name="recherche" placeholder="Recherche.."> 
+        <a href="">
+            <img class="logoLoupe" src="./images/loupe.png" alt="recherche" title="Rechercher">
+        </a>
+    </div>
+    <div class="divPanierProfil">
+        <a href=""> <!-- Lien panier -->
+            <h2>Panier</h2>
+        </a>
+        <a href=""> <!-- Lien profil -->
+            <img class="logoProfil" src="./images/profil.png" onmouseover=passageDeLaSouris(this); onmouseout=departDeLaSouris(this); />
+        </a>
+    </div>
+</div>
+<hr>

5cee1b20d99ce6d4f92c2788bbfd2c1177d2c84c
oui_ <104625385+ouiUnderscore@users.noreply.github.com>
2022-10-21 11:40:56 +0200

    Add files via upload

--- page_accueil/connexion.php
+++ page_accueil/connexion.php
@@ -1,20 +1,26 @@
 <?php require("header.php"); ?>
         </header>
         <main>
-            <section>
-                <h2>Connexion</h2>
-                <img src="./images/logo.png" alt="logoAlizon" title="Accueil" class="logoAlizon">
-                <form action="credits.php" method="post">
-                    <label>Nom d'utiliseur ou adresse mail:</label>
-                    <input type="text" name="identifiant" required="required"/>
-                    <label>Mot de passe: </label>
-                    <input type="password" name="motDePasse" required="required"/>
-                    <input type="checkbox" name="souvenirMoi"/>
-                    <label>Se souvenir de moi</label>
-                    <input type="submit" value="Valider"/>
-                </form>
-                <a href="incription.php">Je n'ai pas de compte</a>
-            </section>
-            <a href="">Mot de passe oublié ?</a>
+            <div class="logoInscription">
+                <img src="./images/logo_noir.png" alt="logoAlizon" title="Accueil">
+            </div>
+            <div class="divCredit">
+                <div class="sectionCredit">
+                    <h2>Connexion</h2>
+                    <form action="credits.php" method="post">
+                        <label>Nom d'utilisateur ou adresse mail:</label>
+                        <input type="text" name="identifiant" required="required"/>
+                        <label>Mot de passe: </label>
+                        <input type="password" name="motDePasse" required="required"/>
+                        <div>
+                            <input type="checkbox"/>
+                            <label>Se souvenir de moi</label>
+                        </div>
+                        <input type="submit" value="Se connecter"/>
+                    </form>
+                    <a href="incription.php">Je n'ai pas de compte</a>
+                </div>
+                <a href="">Mot de passe oublié ?</a>
+            </div>
         </main>
 <?php require("footer.php"); ?>
--- page_accueil/header.php
+++ page_accueil/header.php
@@ -9,12 +9,12 @@
     <body>
         <header>
             <div>
-                <a class="Alizon" href="index.html"> <!-- Lien accueil -->
+                <a class="Alizon" href="index.php"> <!-- Lien accueil -->
                     <img src="./images/logo.png" alt="logoAlizon" title="Accueil" class="logoAlizon">
                     <h1>Alizon</h1>
                 </a>
                 <div class="divRecherche">
-                    <input class="champsRecherche" type="search" name="recherche" placeholder="Recherche.."> 
+                    <input class="champsRecherche" type="text" name="recherche" placeholder="Recherche.."> 
                     <a href="">
                         <img class="logoLoupe" src="./images/loupe.png" alt="recherche" title="Rechercher">
                     </a>
@@ -23,7 +23,7 @@
                     <a href=""> <!-- Lien panier -->
                         <h2>Panier</h2>
                     </a>
-                    <a href=""> <!-- Lien profil -->
+                    <a href="connexion.php"> <!-- Lien profil -->
                         <img class="logoProfil" src="./images/profil.png" onmouseover=passageDeLaSouris(this); onmouseout=departDeLaSouris(this); />
                     </a>
                 </div>

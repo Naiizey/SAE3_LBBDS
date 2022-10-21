e7d28f9d821131c3d4ab4a81a923f7c5755ae42f
Mathis <72527059+Taximin@users.noreply.github.com>
2022-10-21 08:52:17 +0200

    Add files via upload

---
+++ page_accueil/footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <div>
+        <a href=""> <!-- Lien mentions légales -->
+            <p>Mentions légales</p>
+        </a>
+        <a href=""> <!-- Lien contacts -->
+            <p>Contacts</p>
+        </a>
+        <a href=""> <!-- Lien réseaux sociaux -->
+            <p>Réseaux sociaux</p>
+        </a>
+        <a href=""> <!-- Lien aide -->
+            <p>Besoin d'aide ?</p>
+        </a>
+    </div>
+    <p>&copy; 2022 Alizon.bzh et ses affiliés</p>
+</footer>
---
+++ page_accueil/script.js
@@ -0,0 +1,10 @@
+//Fonctions pour le changement d'images lors du hover sur le profil
+//Affecte la nouvelle image lorsque la souris survole l'élément
+function passageDeLaSouris(element) {
+    element.setAttribute('src', './images/profil_hover.png');
+    }
+
+//Affecte l'image de départ lorsque la souris ne survole plus l'élément
+function departDeLaSouris(element) {
+    element.setAttribute('src', './images/profil.png');
+}

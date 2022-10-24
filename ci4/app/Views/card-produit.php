009e8152b379b9f39d68d50b4e73b230c613ec6b
Temnate <nathan.barre035@gmail.com>
2022-10-25 00:01:43 +0200

    Fin de visuel carte, mais manque système étoile

--- ci4/app/Views/card-produit.php
+++ ci4/app/Views/card-produit.php
@@ -1,5 +1,5 @@
 <?php
-$prod=array('lien'=>'rien','contenu'=>"ddddddddd", 'prix'=>3.90);
+$prod=array('lien'=>'ressources/images.jpg','contenu'=>"ddddddddd", 'prix'=>3.90);
 if (str_contains($prod['contenu'],"\n"))
 {
     if(str_contains($prod['contenu'],"\n"))
@@ -18,24 +18,28 @@ if (str_contains($prod['contenu'],"\n"))
         $prod['contenu']=str_split($prod['contenu'],51)."...";  
     }
 
-    $prod['prix']=number_format($prod['prix'],5);
-    
-
-    
 }
+$prod['prix']=sprintf("%5.2f",floor($prod['prix']*100)/100); 
 
 function notationEtoile(){
-    return "⭐⭐⭐⭐⭐";
+    $retour="";
+    for($i=0;$i<5;$i++){
+        $retour.="<img class='star' src='".base_url()."/images/Star-empty.svg'/>";
+    }
+    return $retour;
 }
 
 function cardProduit($prod){ 
     ob_start(); ?>
     <div class="card-produit-ext">
     <div class="card-produit">
-        <div class="image-card"><?= $prod['lien']?></div>
+
+        <div class="image-card" style="background-image: url(<?= base_url().'/'.$prod['lien']?>);"></div>
+
         <div class="notation-card"><?= notationEtoile() ?></div>
         <div class="contain-libelle"><p class="libelle"><?= $prod['contenu']?></p></div>
         <p class="prix-card"><?= $prod['prix']?>€</p>
+
     </div>
     </div>
     <?php return ob_get_clean();
@@ -63,6 +67,10 @@ function cardProduit($prod){
         height: 25px;
 
     }
+    .star{
+        /*TODO: voir pour la couleur dans les étoiles*/
+        margin: 0 1px;
+    }
     .contain-libelle{
         min-height:85px;
         max-height:85px;
@@ -78,6 +86,14 @@ function cardProduit($prod){
         margin-bottom: 0;
         word-wrap: break-word;
     }
+    /*TODO: sauvegarder dans bdd position */
+    div.image-card{
+        
+        background-position: 0;
+    }
+    /* Star 1 */
+
+
 
     .prix-card{
         text-align: center;

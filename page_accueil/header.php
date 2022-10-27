8d4619512f597b5450d926719680cb6f1a158a7f
Naiizey <79869581+Naiizey@users.noreply.github.com>
2022-10-27 18:54:18 +0200

    Header with svgs

--- page_accueil/header.php
+++ page_accueil/header.php
@@ -28,34 +28,31 @@
             } 
             else
             {
-                $newDivHeader = '<div class="divRecherche">
-                                    <input class="champsRecherche" type="text" name="recherche" placeholder="Recherche.."> 
-                                    <a href="">
-                                        <img class="logoLoupe" src="./images/header/loupe.png" alt="recherche" title="Rechercher">
-                                    </a>
-                                </div>';
-            }    
+                $test = file_get_contents("images/header/loupe.svg");
+                $newDivHeader = "<div class='divRecherche'>
+                                    <input class='champsRecherche' type='text' name='recherche' placeholder='Recherche..'>
+                                    <a href=''>".$test."</a>
+                                </div>";
+            }
         ?>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <link rel="stylesheet" type="text/css" href="style.css" />
         <title>Page d'accueil</title>
     </head>
     <body>
         <header>
             <div class="divHeaderAlizon">
                 <a class="lienAlizon" href="index.php"> <!-- Lien accueil -->
-                    <img src="./images/header/logo.png" alt="logoAlizon" title="Accueil" class="logoAlizon">
+                    <?php include("./images/header/logo.svg")?>
                     <h1>Alizon</h1>
                 </a>
-                <?php 
-                    echo $newDivHeader;
-                ?>
+                <?php echo $newDivHeader ?>
                 <div class="divPanierProfil">
                     <a href="panier.php"> <!-- Lien panier -->
                         <h2>Panier</h2>
                     </a>
                     <a class="lienConnexion" href="connexion.php">
-                        <img class="logoProfil" src="./images/header/profil.png" title="logo_connexion" alt="logo_connexion" onmouseover=passageDeLaSouris(this); onmouseout=departDeLaSouris(this);>
+                        <?php include("./images/header/profil.svg")?>
                     </a>
                 </div>
             </div> 

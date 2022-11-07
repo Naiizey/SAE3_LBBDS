9b874717bc19f80273b1ebab05f652a1258d42c8
Taximin <adryallh@gmail.com>
2022-11-07 11:47:52 +0100

    panier retouches

--- ci4/app/Views/page_accueil/panier.php
+++ ci4/app/Views/page_accueil/panier.php
@@ -5,21 +5,23 @@
                     if ($controller == "panierVide")
                     {
                         echo   '<section class="sectionPanier">
-                                    <div class="divPanierHeader">
-                                        <h2>Votre panier est vide</h2>
-                                    </div>
-                                    <hr>
                                     <div>
-                                        <h3 class="h3PasArticlePanier">Vous n’avez aucun article dans votre panier. <br> <a href="index.php" class="lienPasArticlePanier">Cliquez ici</a>, pour continuer vos recherches.</h3>';
+                                        <div class="divPanierHeader">
+                                            <h2>Votre panier est vide</h2>
+                                        </div>
+                                        <hr>
+                                        <div>
+                                            <h3 class="h3PasArticlePanier">Vous n’avez aucun article dans votre panier. <br> <a href="index.php" class="lienPasArticlePanier">Cliquez ici</a>, pour continuer vos recherches.</h3>';
 
-                        $connecte = false;
+                        $connecte = true;
                         if ($connecte == false)
                         {
                             echo "<h3 class='h3PasArticlePanier'>Connectez-vous pour récupérer votre panier.</h3>";
                             echo "<a href=connexion.php class='lienPanier'>Se connecter</a>";
                         } 
                         
-                        echo   '    </div>
+                        echo   '        </div>
+                                    </div>
                                 </section>
                                 <aside>
                                     <h2>Sous-total (0 article) : 00,00€</h2>
@@ -30,18 +32,47 @@
                     {
                         echo   '<section class="sectionPanier">
                                     <div class="divPanierHeader">
-                                        <h2>Votre panier</h2>
-                                        <h3>Prix</h3>
+                                            <h2>Votre panier</h2>
+                                            <h3>Prix</h3>
                                     </div>
-                                    <hr>
-                                    <article class="articlePanier">
-                                        <a href="">
-                                            <img src="./images/art1.png" alt="article 1" title="Article 1">
-                                            <div>
-                                                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a ad qui cumque ...</h2>
-                                                <p class="panierDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eveniet facilis maxime veniam ... </p>
-                                            </div>
-                                        </a>
+                                    <div class="divPanierArticles">
+                                        <hr>
+                                        <article class="articlePanier">
+                                            <a href="">
+                                                <img src="./images/art1.png" alt="article 1" title="Article 1">
+                                                <div>
+                                                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a ad qui cumque ...</h2>
+                                                    <p class="panierDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eveniet facilis maxime veniam ... </p>
+                                                </div>
+                                            </a>
+                                                <div class="divQuantite">
+                                                    <p>Quantité</p>
+                                                    <select name="quantite">
+                                                        <option value="0">0 (Supprimer)</option>
+                                                        <option value="1" selected>1</option>
+                                                        <option value="2">2</option>
+                                                        <option value="3">3</option>
+                                                        <option value="4">4</option>
+                                                        <option value="5">5</option>
+                                                        <option value="6">6</option>
+                                                        <option value="7">7</option>
+                                                        <option value="8">8</option>
+                                                        <option value="9">9</option>
+                                                        <option value="10+">10+</option>
+                                                    </select>
+                                                    <a href="">Supprimer</a>
+                                                </div>
+                                                <h3>19,45€</h3>
+                                        </article>
+                                        <hr>
+                                        <article class="articlePanier">
+                                            <a href="">
+                                                <img src="./images/art2.png" alt="article 2" title="Article 2">
+                                                <div>
+                                                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+                                                    <p class="panierDescription">Lorem ipsum dolor sit amet consectetur, adipisicing elit. </p>
+                                                </div>
+                                            </a>
                                             <div class="divQuantite">
                                                 <p>Quantité</p>
                                                 <select name="quantite">
@@ -59,37 +90,10 @@
                                                 </select>
                                                 <a href="">Supprimer</a>
                                             </div>
-                                            <h3>19,45€</h3>
-                                    </article>
-                                    <hr>
-                                    <article class="articlePanier">
-                                        <a href="">
-                                            <img src="./images/art2.png" alt="article 2" title="Article 2">
-                                            <div>
-                                                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
-                                                <p class="panierDescription">Lorem ipsum dolor sit amet consectetur, adipisicing elit. </p>
-                                            </div>
-                                        </a>
-                                        <div class="divQuantite">
-                                            <p>Quantité</p>
-                                            <select name="quantite">
-                                                <option value="0">0 (Supprimer)</option>
-                                                <option value="1" selected>1</option>
-                                                <option value="2">2</option>
-                                                <option value="3">3</option>
-                                                <option value="4">4</option>
-                                                <option value="5">5</option>
-                                                <option value="6">6</option>
-                                                <option value="7">7</option>
-                                                <option value="8">8</option>
-                                                <option value="9">9</option>
-                                                <option value="10+">10+</option>
-                                            </select>
-                                            <a href="">Supprimer</a>
-                                        </div>
-                                        <h3>35,55€</h3>
-                                    </article>
-                                    <hr>
+                                            <h3>35,55€</h3>
+                                        </article>
+                                        <hr>
+                                    </div>
                                     <div class="divPanierFooter"> 
                                         <a href="">Vider le panier</a>
                                         <h2>Sous-total (2 articles) : 55,00€</h2>
@@ -102,41 +106,46 @@
                     }
                 ?> 
             </div>
-            <section class="sectionRecommandationsPanier">
-                <h2>Recommandations</h2>
-                <hr>
-                <ul>
-                    <li>
-                        <a href="">
-                            <img src="./images/art1.png" alt="article 1" title="Article 1">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="./images/art2.png" alt="article 2" title="Article 2">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="./images/art3.png" alt="article 3" title="Article 3">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="./images/art4.png" alt="article 4" title="Article 4">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="./images/art5.png" alt="article 5" title="Article 5">
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            <img src="./images/art5.png" alt="article 6" title="Article 6">
-                        </a>
-                    </li>
-                </ul>
-            </section>
+            <?php
+                if ($controller == 'panierVide')
+                {
+                    echo    '<section class="sectionRecommandationsPanier">
+                                <h2>Recommandations</h2>
+                                <hr>
+                                <ul>
+                                    <li>
+                                        <a href="">
+                                            <img src="./images/art1.png" alt="article 1" title="Article 1">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="">
+                                            <img src="./images/art2.png" alt="article 2" title="Article 2">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="">
+                                            <img src="./images/art3.png" alt="article 3" title="Article 3">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="">
+                                            <img src="./images/art4.png" alt="article 4" title="Article 4">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="">
+                                            <img src="./images/art5.png" alt="article 5" title="Article 5">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="">
+                                            <img src="./images/art5.png" alt="article 6" title="Article 6">
+                                        </a>
+                                    </li>
+                                </ul>
+                            </section>';
+                }
+            ?>
         </main>
 <?php require("footer.php"); ?>

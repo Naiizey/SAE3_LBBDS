a2197b4b2694b99e47ed1e3d19a872315b2ce2a4
Taximin <adryallh@gmail.com>
2022-11-09 14:29:49 +0100

    panier dynamique!

--- ci4/app/Views/page_accueil/panier.php
+++ ci4/app/Views/page_accueil/panier.php
@@ -35,14 +35,18 @@
                                         <div class="divPanierHeader">
                                                 <h2>Votre panier</h2>
                                                 <h3>Prix</h3>
-                                        </div>
-                                        <hr>
+                                        </div>';
+
+                        foreach ($produits as $produit)
+                        {
+                            //print_r($produit);
+                            echo       '<hr>
                                         <article class="articlePanier">
                                             <a href="">
-                                                <img src="./images/art1.png" alt="article 1" title="Article 1">
+                                                <img src="' . $produit -> lienimage . '" alt="article 1" title="Article 1">
                                                 <div>
-                                                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a ad qui cumque ...</h2>
-                                                    <p class="panierDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eveniet facilis maxime veniam ... </p>
+                                                    <h2>' . $produit -> intitule . '</h2>
+                                                    <p class="panierDescription">' . $produit -> description . '</p>
                                                 </div>
                                             </a>
                                                 <div class="divQuantite">
@@ -62,121 +66,10 @@
                                                     </select>
                                                     <a href="">Supprimer</a>
                                                 </div>
-                                                <h3>19,45€</h3>
-                                        </article>
-                                        <hr>
-                                        <article class="articlePanier">
-                                            <a href="">
-                                                <img src="./images/art1.png" alt="article 1" title="Article 1">
-                                                <div>
-                                                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a ad qui cumque ...</h2>
-                                                    <p class="panierDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eveniet facilis maxime veniam ... </p>
-                                                </div>
-                                            </a>
-                                                <div class="divQuantite">
-                                                    <p>Quantité</p>
-                                                    <select name="quantite">
-                                                        <option value="0">0 (Supprimer)</option>
-                                                        <option value="1" selected>1</option>
-                                                        <option value="2">2</option>
-                                                        <option value="3">3</option>
-                                                        <option value="4">4</option>
-                                                        <option value="5">5</option>
-                                                        <option value="6">6</option>
-                                                        <option value="7">7</option>
-                                                        <option value="8">8</option>
-                                                        <option value="9">9</option>
-                                                        <option value="10+">10+</option>
-                                                    </select>
-                                                    <a href="">Supprimer</a>
-                                                </div>
-                                                <h3>19,45€</h3>
-                                        </article>
-                                        <hr>
-                                        <article class="articlePanier">
-                                            <a href="">
-                                                <img src="./images/art1.png" alt="article 1" title="Article 1">
-                                                <div>
-                                                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a ad qui cumque ...</h2>
-                                                    <p class="panierDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eveniet facilis maxime veniam ... </p>
-                                                </div>
-                                            </a>
-                                                <div class="divQuantite">
-                                                    <p>Quantité</p>
-                                                    <select name="quantite">
-                                                        <option value="0">0 (Supprimer)</option>
-                                                        <option value="1" selected>1</option>
-                                                        <option value="2">2</option>
-                                                        <option value="3">3</option>
-                                                        <option value="4">4</option>
-                                                        <option value="5">5</option>
-                                                        <option value="6">6</option>
-                                                        <option value="7">7</option>
-                                                        <option value="8">8</option>
-                                                        <option value="9">9</option>
-                                                        <option value="10+">10+</option>
-                                                    </select>
-                                                    <a href="">Supprimer</a>
-                                                </div>
-                                                <h3>19,45€</h3>
-                                        </article>
-                                        <hr>
-                                        <article class="articlePanier">
-                                            <a href="">
-                                                <img src="./images/art1.png" alt="article 1" title="Article 1">
-                                                <div>
-                                                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a ad qui cumque ...</h2>
-                                                    <p class="panierDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eveniet facilis maxime veniam ... </p>
-                                                </div>
-                                            </a>
-                                                <div class="divQuantite">
-                                                    <p>Quantité</p>
-                                                    <select name="quantite">
-                                                        <option value="0">0 (Supprimer)</option>
-                                                        <option value="1" selected>1</option>
-                                                        <option value="2">2</option>
-                                                        <option value="3">3</option>
-                                                        <option value="4">4</option>
-                                                        <option value="5">5</option>
-                                                        <option value="6">6</option>
-                                                        <option value="7">7</option>
-                                                        <option value="8">8</option>
-                                                        <option value="9">9</option>
-                                                        <option value="10+">10+</option>
-                                                    </select>
-                                                    <a href="">Supprimer</a>
-                                                </div>
-                                                <h3>19,45€</h3>
-                                        </article>
-                                        <hr>
-                                        <article class="articlePanier">
-                                            <a href="">
-                                                <img src="./images/art1.png" alt="article 1" title="Article 1">
-                                                <div>
-                                                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum a ad qui cumque ...</h2>
-                                                    <p class="panierDescription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus eveniet facilis maxime veniam ... </p>
-                                                </div>
-                                            </a>
-                                                <div class="divQuantite">
-                                                    <p>Quantité</p>
-                                                    <select name="quantite">
-                                                        <option value="0">0 (Supprimer)</option>
-                                                        <option value="1" selected>1</option>
-                                                        <option value="2">2</option>
-                                                        <option value="3">3</option>
-                                                        <option value="4">4</option>
-                                                        <option value="5">5</option>
-                                                        <option value="6">6</option>
-                                                        <option value="7">7</option>
-                                                        <option value="8">8</option>
-                                                        <option value="9">9</option>
-                                                        <option value="10+">10+</option>
-                                                    </select>
-                                                    <a href="">Supprimer</a>
-                                                </div>
-                                                <h3>19,45€</h3>
-                                        </article>
-                                        <hr>
+                                                <h3>' . $produit -> prixTtc . '€</h3>
+                                        </article>';
+                        }                
+                        echo       '    <hr>
                                     </div>
                                     <div class="divPanierFooter"> 
                                         <a href="">Vider le panier</a>

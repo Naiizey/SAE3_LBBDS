3be818b2b1b32bbbe8b7fbc839471c7929132eff
Taximin <adryallh@gmail.com>
2022-11-09 13:47:14 +0100

    un peu de back-end panier

--- ci4/app/Models/ProduitPanierModel.php
+++ ci4/app/Models/ProduitPanierModel.php
@@ -7,7 +7,7 @@ use \App\Entities\Produit as Produit;
 use CodeIgniter\Model;
 
 
-class ProduitCatalogueModel extends Model
+class ProduitPanierModel extends Model
 {
     protected $table      = 'sae3.produit_panier';
     protected $primaryKey = 'id';

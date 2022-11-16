e4fad65b25af0870009445c02d12da8534c782f6
oui_ <emilie.zammit29@gmail.com>
2022-11-16 17:45:52 +0100

    début back mdp oublié

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -44,7 +44,8 @@ $routes->set404Override();
     $routes->post('/connexion', 'Home::connexion');
     $routes->get('/inscription', 'Home::inscription');
     $routes->post('/inscription', 'Home::inscription');
-    $routes->get('/mdpOublie', 'Home::mdpOublie');
+    $routes->get('/mdpOublie', 'MdpOublie::mdpOublie');
+    $routes->post('/obtenirCode', 'MdpOublie::obtenirCode');
 
 
     $routes->get('/produit', 'Home::produit');

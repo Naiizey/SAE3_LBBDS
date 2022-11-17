2b74057e0cc512020657261d66b920264f892e32
oui_ <emilie.zammit29@gmail.com>
2022-11-17 12:15:15 +0100

    merge de merde

--- ci4/public/js/script.js
+++ ci4/public/js/script.js
@@ -234,3 +234,38 @@ function updatePriceTotal() {
     prixTotTab[0].textContent = sommeTot;
     prixTotTab[1].textContent = sommeTot; 
 }
+
+/*
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃                           Liens aux lignes de lstCommandes                          ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+*/
+
+var lignes=document.getElementsByClassName("lignesCommandes");
+var numCommandes=document.getElementsByClassName("numCommandes");
+
+for (let numLigne of lignes) {
+    var ligneA=lignes.item(numLigne);
+    var commandeA=numCommandes[numLigne];
+    ligneA.addEventListener("click", lienLigne);
+}
+/*
+function lienLigne(element) {
+    window.location.href = `/commande/${(commandeA.textContent)}`;
+}
+*/
+
+
+/*
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃                                      CGU                                        ┃
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+*/
+var lienCGU = document.getElementsByClassName("lienCGU");
+lienCGU[0].addEventListener("click", test);
+
+/*document.querySelector(".mentionLegales > p").style.color = "red";*/
+
+function test(event) {
+    event.preventDefault();    
+}

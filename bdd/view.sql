33fc72168e0e039583b0a652edeab53f2ee475cb
sc2marines <alexandre.foll35@gmail.com>
2022-11-18 16:47:27 +0100

    added triiger notworking

--- bdd/view.sql
+++ bdd/view.sql
@@ -50,7 +50,7 @@ CREATE OR REPLACE FUNCTION retourneEtatLivraison(entree_num_panier int) RETURNS 
     DECLARE
         row sae3._commande%ROWTYPE;
     BEGIN
-        SELECT date_expedition, date_plateformeReg, date_plateformeLoc, date_arriv  INTO row FROM sae3._commande where num_panier=entree_num_panier;
+        SELECT date_expedition, date_plateformeReg, date_plateformeLoc, date_arriv  INTO row FROM sae3._commande where num_commande=entree_num_panier;
 
         IF row.date_expedition is null then
             return 1;
@@ -73,6 +73,6 @@ CREATE OR REPLACE FUNCTION retourneEtatLivraison(entree_num_panier int) RETURNS 
 
 --SELECT * FROM _commande NATURAL JOIN _panier NATURAL JOIN _refere NATURAL JOIN _produit;
 CREATE OR REPLACE VIEW commande_list_vendeur AS
-    SELECT num_commande,num_compte,intitule_prod,date_commande,date_arriv, prix_ht, prix_ttc, qte_panier FROM _commande NATURAL JOIN _panier NATURAL JOIN _refere NATURAL JOIN _produit NATURAL JOIN _panier_client;
+    SELECT num_commande,retourneEtatLivraison(num_commande),num_compte,intitule_prod,date_commande,date_arriv, prix_ht, prix_ttc, qte_panier FROM _commande NATURAL JOIN _panier NATURAL JOIN _refere NATURAL JOIN _produit NATURAL JOIN _panier_client;
     
 SELECT * FROM commande_list_vendeur;

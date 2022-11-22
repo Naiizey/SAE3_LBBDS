8f613501e9401876e16781bec0e3f3050e91bdc8
sc2marines <alexandre.foll35@gmail.com>
2022-11-22 18:43:26 +0100

    page details produits

---
+++ ci4/app/Views/panier/details.php
@@ -0,0 +1,20 @@
+<?php require(__DIR__."/../page_accueil/header.php"); ?>
+        </header>
+        <main >
+            <section class="main-block" class="detail-cmd">
+                <h3>Commande n°15263890891</h3>
+                <div id="bar">   
+                    <div class="progress-bar-ok"></div>
+                    <div>                    
+                        <div class="point-ok"></div>
+                        <div class="point-ok"></div>
+                        <div class="point-ok"></div>
+                        <div class="point-ok"></div>
+                    </div>
+
+                </div>
+                <div class="secondary-block"></div>
+                    
+            </section>
+        </main>
+<?php require(__DIR__."/../page_accueil/footer.php"); ?>

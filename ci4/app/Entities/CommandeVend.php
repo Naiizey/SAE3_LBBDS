55b3c890dbf240a0725a3971482fd2968b24561a
Liebherru <evan22140@gmail.com>
2022-11-23 18:48:03 +0100

    LstCommandesVendeur presque fini

--- ci4/app/Entities/CommandeVend.php
+++ ci4/app/Entities/CommandeVend.php
@@ -4,5 +4,30 @@ use CodeIgniter\Entity\Entity;
 
 class CommandeVend extends Entity
 {
-   
+  public function etatString() : String
+  {
+      switch ($this->etat) {
+         case 0:
+            return 'En attente';
+            break;
+         case 1:
+            return 'En route';
+            break;
+         case 2:
+            return 'Livrée';
+            break;
+         case 3:
+            return 'Annulée';
+            break;
+         case 4:
+            return 'Retour';
+            break;
+         case 5:
+            return 'Terminée';
+            break;
+         default:
+            return 'Erreur';
+            break;
+      }
+  }
 }

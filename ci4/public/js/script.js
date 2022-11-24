fa6b89d6d5f0fe190ff016aff68e705cdca40227
Naiizey <79869581+Naiizey@users.noreply.github.com>
2022-11-24 11:39:43 +0100

    pixel price range

--- ci4/public/js/script.js
+++ ci4/public/js/script.js
@@ -378,40 +378,52 @@ function cataloguePrice(){
     priceInput = document.querySelectorAll(".price-range input:not(.range-input input)"),
     range = document.querySelector(".slider .progress");
     let priceGap = 100;
+
+    priceInput.forEach(() => {
+        window.addEventListener("load", fctPriceInput);
+    });
+    rangeInput.forEach(() => {
+        window.addEventListener("load", fctRangeInput);
+    });
+
     priceInput.forEach((input) => {
-        input.addEventListener("input", (e) => {
-            let minPrice = parseInt(priceInput[0].value),
-            maxPrice = parseInt(priceInput[1].value);
-            if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
-                if (e.target.id === "prix_min") {
-                    rangeInput[0].value = minPrice;
-                    range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
-                    console.log(minPrice + " | " + maxPrice);
-                } else if(e.target.id === "prix_max") {
-                    rangeInput[1].value = maxPrice;
-                    range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
-                }
-            }
-        });
+        input.addEventListener("input", fctPriceInput);
     });
     rangeInput.forEach((input) => {
-        input.addEventListener("input", (e) => {
-            let minVal = parseInt(rangeInput[0].value),
-            maxVal = parseInt(rangeInput[1].value);
-            if (maxVal - minVal < priceGap) {
-            if (e.target.className === "range-min") {
-                rangeInput[0].value = maxVal - priceGap;
-            } else {
-                rangeInput[1].value = minVal + priceGap;
+        input.addEventListener("input", fctRangeInput);
+    });
+
+    function fctPriceInput(e){
+        let minPrice = parseInt(priceInput[0].value),
+        maxPrice = parseInt(priceInput[1].value);
+        if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
+            if (e.target.id === "prix_min") {
+                rangeInput[0].value = minPrice;
+                range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+                console.log(minPrice + " | " + maxPrice);
+            } else if(e.target.id === "prix_max") {
+                rangeInput[1].value = maxPrice;
+                range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
             }
-            } else {
+        }
+    }
+
+    function fctRangeInput(e){
+        let minVal = parseInt(rangeInput[0].value),
+        maxVal = parseInt(rangeInput[1].value);
+        if (maxVal - minVal < priceGap) {
+        if (e.target.className === "range-min") {
+            rangeInput[0].value = maxVal - priceGap;
+        } else {
+            rangeInput[1].value = minVal + priceGap;
+        }
+        } else {
             priceInput[0].value = minVal;
             priceInput[1].value = maxVal;
             range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
             range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
-            }
-        });
-    });
+        }
+    }
 }
 
 

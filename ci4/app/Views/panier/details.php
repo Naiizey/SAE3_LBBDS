e4515eca8ccf5b2c3d9341011cc36ecdb1c01fb5
oui_ <emilie.zammit29@gmail.com>
2022-11-24 10:40:16 +0100

    bar progress detail

--- ci4/app/Views/panier/details.php
+++ ci4/app/Views/panier/details.php
@@ -1,20 +1,21 @@
 <?php require(__DIR__."/../page_accueil/header.php"); ?>
         </header>
         <main >
-            <section class="main-block" class="detail-cmd">
+            <section class="main-block">
                 <h3>Commande n°15263890891</h3>
-                <div id="bar">   
-                    <progress class="progress-bar-ok" value="70" max="100">70 %</progress>       
-                    <div class="button">
+                <div class="bar">   
+                    <progress class="progress-bar-ok" value="50" max="100"></progress>       
+                    <div class="buttonProgress">
+                        <div class="point-ok"></div>
                         <div class="point-ok"></div>
                         <div class="point-ok"></div>
                         <div class="point-ok"></div>
                         <div class="point-ok"></div>
                     </div>
+                </div>
+                <div class="secondary-block">
                     
                 </div>
-                <div class="secondary-block"></div>
-                    
             </section>
         </main>
 <?php require(__DIR__."/../page_accueil/footer.php"); ?>

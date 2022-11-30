4ff64cb41b7accec27c475a2cbf6459940a4991b
oui_ <emilie.zammit29@gmail.com>
2022-11-30 10:00:23 +0100

    back detail commande bientot fini uwu

--- bdd/view.sql
+++ bdd/view.sql
@@ -117,7 +117,8 @@ CREATE OR REPLACE VIEW commande_list_client AS
 SELECT * FROM commande_list_client;
 
 CREATE OR REPLACE VIEW commande_list_produits_client AS
-    SELECT num_commande,num_compte,date_commande,date_arriv,(prix_ttc*qte_panier) prix_ttc,(prix_ht*qte_panier) prix_ht,qte_panier qte, retourneEtatLivraison(num_commande) etat FROM _commande NATURAL JOIN _panier NATURAL JOIN _refere NATURAL JOIN _produit NATURAL JOIN _panier_client;
+    SELECT num_commande,intitule_prod,num_compte,date_commande,date_arriv,(prix_ttc*qte_panier) prix_ttc,(prix_ht*qte_panier) prix_ht,qte_panier qte, retourneEtatLivraison(num_commande) etat FROM _commande NATURAL JOIN _panier NATURAL JOIN _refere NATURAL JOIN _produit NATURAL JOIN _panier_client;
+SELECT * FROM commande_list_produits_client;
 
 CREATE OR REPLACE VIEW insert_commande AS
     SELECT num_commande,num_compte,id_a FROM _commande NATURAL JOIN _panier_client;
--- ci4/public/js/script.js
+++ ci4/public/js/script.js
@@ -462,17 +462,27 @@ function switchEtatFiltre(list){
 */
 
 function barreProgression() {
+    let pointProgress = document.getElementsByClassName("pointProgress");
+    let preuveLivraison = document.querySelector(".preuveLivraison");
+    let progressBar = document.querySelector(".progress-bar-ok");
+
+    if (progressBar.value < 5) {
+        progressBar.value = (progressBar.value - 1)*25 + 12,5;
+    } else {
+        progressBar.value = 100;
+    }
+
     for (let index = 0; index < 5; index++) {
-        if (index*25 <= document.querySelector(".progress-bar-ok").value) {
-            document.getElementsByClassName("pointProgress")[index].classList.add("point-ok");
+        if (index*25 <= progressBar.value) {
+            pointProgress[index].classList.add("point-ok");
         } else {
-            document.getElementsByClassName("pointProgress")[index].classList.add("point-ko");
+            pointProgress[index].classList.add("point-ko");
         }
     }
-    if ((document.getElementsByClassName("pointProgress")[3].classList.contains("point-ok")) && (document.getElementsByClassName("pointProgress")[4].classList.contains("point-ko"))) {
-        document.querySelector(".preuveLivraison").style.backgroundColor = "#BDBFBB";
-        document.querySelector(".preuveLivraison").style.color = "#164F57";
-        document.querySelector(".preuveLivraison").style.cursor = "not-allowed";
+    if (pointProgress[4].classList.contains("point-ko")) {
+        preuveLivraison.style.backgroundColor = "#BDBFBB";
+        preuveLivraison.style.color = "#164F57";
+        preuveLivraison.style.cursor = "not-allowed";
     }
 }
 /*

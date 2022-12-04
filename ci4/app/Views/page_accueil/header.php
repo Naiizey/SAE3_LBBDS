ccd4852d36a75ad4b041f500c30a695c1fe8e91e
oui_ <emilie.zammit29@gmail.com>
2022-12-04 22:03:00 +0100

    lien commandes et puis images carousel

--- ci4/app/Views/page_accueil/header.php
+++ ci4/app/Views/page_accueil/header.php
@@ -113,6 +113,7 @@
                         <div class="divHoverConnexion divConnected">
                             <p class="pNom">Bonjour <?= (session()->get("nom")) ?></p>
                             <a href="<?= base_url()."/espaceClient"?>"><p>Mon profil</p></a>
+                            <a href="<?= base_url()."/commandes"?>"><p>Mes commandes</p></a>
                             <a href="<?= base_url()."/destroy"?>"><p>Se déconnecter</p></a>
                         </div>
                     <?php else: ?>

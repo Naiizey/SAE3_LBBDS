0f4a2efee98a4b0e461ce57e1716ede9d715b51e
Naiizey <79869581+Naiizey@users.noreply.github.com>
2022-12-05 10:50:44 +0100

    € on produit

--- ci4/app/Views/page_accueil/produit.php
+++ ci4/app/Views/page_accueil/produit.php
@@ -105,8 +105,8 @@
                                     ?>">
                                 </div>
                                 <div>
-                                    <h3><?= "Prix (HT): " . $prod -> prixht ?></h3>
-                                    <h3><?= '(TTC): ' . $prod -> prixttc ?></h3>
+                                    <h3><?= "Prix (HT): " . $prod -> prixht . "€"?></h3>
+                                    <h3><?= '(TTC): ' . $prod -> prixttc . "€"?></h3>
                                 </div>
                                 <button type="submit">Ajouter au panier</button>
                             </form>

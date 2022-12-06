bee058ffb53996f1a715caeb2817c36e74021bbd
oui_ <emilie.zammit29@gmail.com>
2022-12-06 17:16:08 +0100

    corrigé

--- ci4/app/Views/page_accueil/footer.php
+++ ci4/app/Views/page_accueil/footer.php
@@ -5,7 +5,7 @@
                     </a>
                 </div>
                 <h2>Mentions légales</h2>
-                <p>Tout personne très peu sympathique tentant de récupérer nos mentions légales se verra dénoncé auprès de Bertrand de Villeneuve et sera notifié oralement de notre ingratitude envers cet acte. Dans le cas échéant, il s'engage à nous verser une compensation monétaire d'une hauteur de 100€ pour le dommage intelectuel et moral effectué.</p>
+                <p>Toute personne très peu sympathique tentant de récupérer nos mentions légales se verra dénoncé auprès de Bertrand de Villeneuve et sera notifié oralement de notre ingratitude envers cet acte. Dans le cas échéant, il s'engage à nous verser une compensation monétaire d'une hauteur de 100€ pour le dommage intellectuel et moral effectué.</p>
                 <p>Ceci n'est pas une blague.</p>
                 <p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Les vraies mentions légales</a></p>
                 <p></p>

9727d4fff7b04b02b516d3980e3d72b83f6f240d
Temnate <nathan.barre035@gmail.com>
2023-01-04 13:27:05 +0100

    json en bonne voie

--- suiviLivraison/Workspace/json_interpret.c
+++ suiviLivraison/Workspace/json_interpret.c
@@ -27,56 +27,82 @@ typedef struct {
     char * nom;
 
 } proprieteAttendu;
+typedef proprieteAttendu pA_tab[30];
 
-ajoutpA(pA_tab tab, int * offset, int type, char * nom){
+
+void ajoutpA(pA_tab tab, int * offset, int type, char * nom){
     proprieteAttendu pA;
     pA.type=type;
     pA.nom=nom;
     tab[*offset]=pA;
-    offset=offset+1;
+    *offset=*offset+1;
 }
 
-typedef proprieteAttendu pA_tab[30];
 
 int parcours(cJSON *json, pA_tab tab, int * offset){
     switch(json->type){
         case cJSON_Object:
             ajoutpA(tab,offset,cJSON_Object,json->string);
-            if (json->child=NULL) return -1;
-            parcours(json->child,tab,offset);
+            if (json->child==NULL || parcours(json->child,tab,offset) < 0) return -1;
+            
             break;
         case cJSON_Number:
             ajoutpA(tab,offset,cJSON_Number,json->string);
             break;
-        case cJSON_String
-        
+        case cJSON_String:
+            ajoutpA(tab, offset, cJSON_String, json->string);
+            break;
+        default:
+            printf("Erreur: type non raccepté");
+            return -1;
+            break;
+
+    }
+    if(json->next!=NULL){
+        return parcours(json->next,tab,offset);
+    }else{
+        return 0;
     }
     
 }
 
 int buildconfig(){
     char buff[1024];
+    int offset=0;
+    pA_tab tab;
     int fd = open("config.json", O_RDONLY);
-    read(fd,buff,1024);
-    printf("Parsing...");
+    printf("Ouverture...\n");
+    if (fd<0) return -1;
+    printf("Lecture...\n");
+    if (read(fd,buff,1024)<0) return -1;
+    
+    printf("Parsing...\n");
     cJSON *json = cJSON_Parse(buff);
-    if(json == NULL) return -1;
-        
-  
-    if(json->type == cJSON_Object){
-        printf("Objet\n");
-    }else if(json->type == cJSON_Number){
-        printf("Number\n");
+    printf(cJSON_Print(json));
+    if(json == NULL) 
+    {
+     
+        printf("\n");
+        return -1;
     }
 
-  
-    printf("\n");
+        
+    printf("Parcours...%d\n",offset );
+    int retour = parcours(json, tab, &offset);
+    if (retour >= 0){
+        for(int i=0; i<offset ;i++){
+            printf("là %s\n",tab[i].nom);
+        }
+    }
+
+    return retour;
+    
 }
 
 int main(int argc, char const *argv[])
 {
-    creaJson();
-    printf("%ld\n",time(NULL));
+   
+    printf("%d\n", buildconfig());
     
     return 0;
 }

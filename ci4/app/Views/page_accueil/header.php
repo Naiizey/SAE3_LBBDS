8434776d8d3cf80981f034e181f476a15914d0a4
Lucien HERVE <lucienherve@hotmail.com>
2023-01-04 13:32:56 +0100

    ajout de Google Analytics

--- ci4/app/Views/page_accueil/header.php
+++ ci4/app/Views/page_accueil/header.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html lang=fr>
     <head>
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YLXNF3F94Y"></script>
-        <script>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PCVRSM3');</script>
+        <!-- End Google Tag Manager -->
+
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
@@ -72,6 +77,10 @@
                     <?= "class=bodyVendeur" ?>
                 <?php endif;?>
             <?php endif; ?>> 
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PCVRSM3"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <header>
             <div class="divHeaderAlizon 
                 <?php if (isset($role)): ?>

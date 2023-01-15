49f4ebb89d8a102a92e2cb980d27defb4dbca3cd
oui_ <emilie.zammit29@gmail.com>
2023-01-15 18:09:29 +0100

    signalement temporaire

--- ci4/public/js/script.js
+++ ci4/public/js/script.js
@@ -1698,5 +1698,16 @@ function avisProduit()
 }
 
 function signalement(numAvis) {
-    document.querySelector(".divSignalement").style.display = "flex";
+    document.querySelector(".divAvisContenuEtSignal").addEventListener("click", function(event) {
+        event.preventDefault;
+        alert(numAvis);
+        document.getElementById("flag"+numAvis).style.display = "flex";
+    
+        document.querySelector(".divSignalement form div p").addEventListener("click", function() {
+            document.querySelector("#flag"+numAvis).style.display = "none";
+            document.querySelector(".divSignalement").style.display = "none";
+        });
+
+    });
+    return false;
 }

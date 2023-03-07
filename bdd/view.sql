b4347453f95075bd2a69dcb7ab43298250befdf9
sc2marines <alexandre.foll35@gmail.com>
2023-03-07 21:21:28 +0100

    temp

--- bdd/view.sql
+++ bdd/view.sql
@@ -183,8 +183,8 @@ SELECT num_compte numero, email, pseudo identifiant, mot_de_passe motDePasse, nu
 FROM compte_vendeur INNER JOIN _adresse ON _adresse.id_a = compte_vendeur.id_adresse;
 
 --CATALOGUEUR (ADMIN & VENDEUR)
-CREATE OR REPLACE VIEW catalogueur_admin AS
-SELECT id_quidi,num_compte,intitule_prod, prix_ht, prix_ttc, description_prod,logo,moyenne_note_prod FROM _quidi NATURAL JOIN _produit NATURAL JOIN _vendeur;
+CREATE OR REPLACE VIEW glossaire_admin AS
+SELECT id_quidi,num_compte,intitule_prod, prix_ht, prix_ttc, description_prod,logo,moyenne_note_prod FROM _quidi NATURAL JOIN _produit NATURAL JOIN _vendeur NATURAL JOIN _compte ;
 
 CREATE OR REPLACE VIEW catalogueur_vendeur AS 
 SELECT id_quidi,num_compte,numero_rue,nom_rue,code_postal,ville,intitule_prod, prix_ht, prix_ttc, description_prod,pseudo,note_vendeur,numero_siret,tva_intercommunautaire,texte_presentation,logo,moyenne_note_prod FROM _quidi NATURAL JOIN _produit NATURAL JOIN _vendeur NATURAL JOIN _compte INNER JOIN _adresse  ON _vendeur.id_adresse = _adresse.id_a;

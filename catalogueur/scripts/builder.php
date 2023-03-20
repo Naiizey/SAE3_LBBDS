08df774e83a3a9a24b8714ff362579d67df3c7e7
Taximin <adryallh@gmail.com>
2023-03-20 15:58:15 +0100

    je n'ai aucune idée de ce que je fais

--- catalogueur/scripts/builder.php
+++ catalogueur/scripts/builder.php
@@ -3,38 +3,61 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" type="text/css" href="/modele/modele.css" />
         <style>
             @font-face {
                 font-family: "Expletus Sans";
-                src: url("/docker/dependances/fonts/ExpletusSans-Bold.ttf") format("truetype")
+                src: url("dependances/fonts/ExpletusSans-Bold.ttf") format("truetype")
             }
 
             @font-face {
                 font-family: "Montserrat-Medium";
-                src: url("/docker/dependances/fonts/Montserrat-Medium.ttf") format("truetype");
+                src: url("dependances/fonts/Montserrat-Medium.ttf") format("truetype");
             }
 
             @font-face {
                 font-family: "Montserrat-Regular";
-                src: url("/docker/dependances/fonts/Montserrat-Regular.ttf") format("truetype");
+                src: url("dependances/fonts/Montserrat-Regular.ttf") format("truetype");
             }
 
             @font-face {
                 font-family: "Montserrat-Bold";
-                src: url("/docker/dependances/fonts/Montserrat-Bold.ttf") format("truetype");
+                src: url("dependances/fonts/Montserrat-Bold.ttf") format("truetype");
             }
+
+            <?php include("modele/modele.css") ?>
         </style>
         <?php
-            $json = json_decode(file_get_contents("../samples/mono.json"), true);
+            $json = json_decode(file_get_contents("samples/mono.json"), true);
 
-            //print_r($json[0]["id"]);
+            $glossaire = array(
+                "id_quidi" => 1,
+                "num_compte" => 5,
+                "intitule_prod" => "Cola x6",
+                "prix_ht" => 5,
+                "prix_ttc" => 6,
+                "description_prod" => "Nouvelle boisson bretonne au cola, issue de d'une agrigulture bio éthique, bio responsable, bio consciente.",
+                "logo" => "https://webstockreview.net/images/sample-png-images-8.png",
+                "note_vendeur" => 3,
+                "pseudo" => "COBREC_1",
+                "numero_siret" => 123456789,
+                "tva_intercommunautaire" => "FR50123456789",
+                "texte_presentation" => "Bonjour, nous sommes la COBREC",
+                "moyenne_note_prod" => 4.5,
+                "email" => "cobrec1@alizon.net",
+                "numero_rue" => 3,
+                "nom_rue" => "rue de la guerre",
+                "code_postal" => 70000,
+                "lien_image" => "https://images.pexels.com/photos/8879617/pexels-photo-8879617.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                "num_image" => 0,
+                "ville" => "lille"
+            );
+            
         ?>
     </head>
     <body>
         <header>
             <a class="lienAlizon" href=""> <!-- Lien accueil -->
-                <?php include("../docker/dependances/images/header/logo.svg")?>
+                <?php include("dependances/images/header/logo.svg")?>
                 <h1>Alizon</h1>
             </a>
         </header>
@@ -79,14 +102,13 @@
             <section class="sectionProduits">
             <?php foreach ($json as $article): ?>
                 <div class="divUnProduit">
-                    <img src="<?= $article['lien_image']; ?>">
+                    <img src="<?= $article['lienImage']; ?>">
                     <div class="divNomDescription">
-                        <h2><?= $article['intitule_prod']; ?></h2>
+                        <h2><?= $article['intitule']; ?></h2>
                         <p><?= $article['description_prod']; ?></p>
                     </div>
                     <div class="divPrix">
-                        <p><?= $article['prix_ht']; ?>€ HT</p>
-                        <p><?= $article['prix_ttc']; ?>€ TTC</p>
+                        <p><?= $article['prixTtc']; ?>€ TTC</p>
                     </div>
                 </div>
                 <hr>

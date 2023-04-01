ecf952c4bcc252902b9a04948d8738a3aa2617bf
sc2marines <alexandre.foll35@gmail.com>
2023-04-01 17:28:22 +0200

    fix num_compte

--- bdd/insert.sql
+++ bdd/insert.sql
@@ -402,12 +402,19 @@ INSERT INTO _signalement (raison, num_avis, num_compte) VALUES ('Cet avis ne par
     
  -- jusqu'ici
 
-INSERT INTO _quidi VALUES (DEFAULT,1,5);
-INSERT INTO _quidi VALUES (DEFAULT,2,5);
-INSERT INTO _quidi VALUES (DEFAULT,3,5);
-INSERT INTO _quidi VALUES (DEFAULT,4,5);
-INSERT INTO _quidi VALUES (DEFAULT,5,6);
-INSERT INTO _quidi VALUES (DEFAULT,6,6);
-INSERT INTO _quidi VALUES (DEFAULT,7,6);
-INSERT INTO _quidi VALUES (DEFAULT,8,5);
-INSERT INTO _quidi VALUES (DEFAULT,9,5);
+INSERT INTO _quidi(id_quidi,id_prod) VALUES (DEFAULT,1);
+INSERT INTO _quidi(id_quidi,id_prod) VALUES (DEFAULT,2);
+INSERT INTO _quidi(id_quidi,id_prod) VALUES (DEFAULT,3);
+INSERT INTO _quidi(id_quidi,id_prod) VALUES (DEFAULT,4);
+INSERT INTO _quidi(id_quidi,id_prod) VALUES (DEFAULT,5);
+INSERT INTO _quidi(id_quidi,id_prod) VALUES (DEFAULT,6);
+INSERT INTO _quidi(id_quidi,id_prod) VALUES (DEFAULT,7);
+INSERT INTO _quidi(id_quidi,id_prod) VALUES (DEFAULT,8);
+INSERT INTO _quidi(id_quidi,id_prod) VALUES (DEFAULT,9);
+
+INSERT INTO _quidi_vendeur (id_quidi, num_compte) VALUES (1, 5);
+INSERT INTO _quidi_vendeur (id_quidi, num_compte) VALUES (2, 5);
+INSERT INTO _quidi_vendeur (id_quidi, num_compte) VALUES (3, 5);
+INSERT INTO _quidi_vendeur (id_quidi, num_compte) VALUES (4, 5);
+INSERT INTO _quidi_vendeur (id_quidi, num_compte) VALUES (5, 6);
+INSERT INTO _quidi_vendeur (id_quidi, num_compte) VALUES (6, 6);
